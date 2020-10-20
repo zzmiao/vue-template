ebe52f7444484677ecf1176d11db18774904ae2c
miaozhenzhen <1415622690@qq.com>
2020-10-20 13:37:02 +0800

    vue-element

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-//	devServer: {
-//		port: '8090',
-//		host: "localhost",
-//		open: true, //配置浏览器自动访问
-//	}
+	devServer: {
+		port: '8090',
+		host: "localhost",
+		open: true, //配置浏览器自动访问
+	}
 }
